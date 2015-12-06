2f7d37cd0d94e7f91ab57a95d74554da361cf861
Jakob Unterwurzacher <jakobunt@gmail.com>
2015-12-06 18:46:39 +0100

    Get rid of allocations inside xorBlocks
    Before:
      BenchmarkEnc512-2	   30000	     49419 ns/op	  10.36 MB/s
      BenchmarkDec512-2	   30000	     49225 ns/op	  10.40 MB/s
    
    After:
      BenchmarkEnc512-2	   50000	     36037 ns/op	  14.21 MB/s
      BenchmarkDec512-2	   50000	     36255 ns/op	  14.12 MB/s

--- eme.go
+++ eme.go
@@ -29,16 +29,14 @@ func multByTwo(in []byte) (out []byte) {
 	return out
 }
 
-func xorBlocks(in1 []byte, in2 []byte) (out []byte) {
+func xorBlocks(out []byte, in1 []byte, in2 []byte) {
 	if len(in1) != len(in2) {
 		log.Panicf("len(in1)=%d is not equal to len(in2)=%d", len(in1), len(in2))
 	}
 
-	out = make([]byte, len(in1))
 	for i := range in1 {
 		out[i] = in1[i] ^ in2[i]
 	}
-	return out
 }
 
 func transformAES(dst []byte, src []byte, direction int, bc cipher.Block) {
@@ -66,19 +64,21 @@ func TransformEME32(bc cipher.Block, T []byte, P []byte, direction int) (C []byt
 	bc.Encrypt(zero, zero)
 	L := multByTwo(zero)
 
+	PPj := make([]byte, 16)
 	for j := 0; j < m; j++ {
 		Pj := P[j*16 : (j+1)*16]
 		/* PPj = 2**(j-1)*L xor Pj */
-		PPj := xorBlocks(Pj, L)
+		xorBlocks(PPj, Pj, L)
 		/* PPPj = AESenc(K; PPj) */
 		transformAES(C[j*16:(j+1)*16], PPj, direction, bc)
 		L = multByTwo(L)
 	}
 
 	/* MP =(xorSum PPPj) xor T */
-	MP := xorBlocks(C[0:16], T)
+	MP := make([]byte, 16)
+	xorBlocks(MP, C[0:16], T)
 	for j := 1; j < m; j++ {
-		MP = xorBlocks(MP, C[j*16:(j+1)*16])
+		xorBlocks(MP, MP, C[j*16:(j+1)*16])
 	}
 
 	/* MC = AESenc(K; MP) */
@@ -86,19 +86,21 @@ func TransformEME32(bc cipher.Block, T []byte, P []byte, direction int) (C []byt
 	transformAES(MC, MP, direction, bc)
 
 	/* M = MP xor MC */
-	M := xorBlocks(MP, MC)
-
+	M := make([]byte, 16)
+	xorBlocks(M, MP, MC)
+	CCCj := make([]byte, 16)
 	for j := 1; j < m; j++ {
 		M = multByTwo(M)
 		/* CCCj = 2**(j-1)*M xor PPPj */
-		CCCj := xorBlocks(C[j*16:(j+1)*16], M)
+		xorBlocks(CCCj, C[j*16:(j+1)*16], M)
 		copy(C[j*16:(j+1)*16], CCCj)
 	}
 
 	/* CCC1 = (xorSum CCCj) xor T xor MC */
-	CCC1 := xorBlocks(MC, T)
+	CCC1 := make([]byte, 16)
+	xorBlocks(CCC1, MC, T)
 	for j := 1; j < m; j++ {
-		CCC1 = xorBlocks(CCC1, C[j*16:(j+1)*16])
+		xorBlocks(CCC1, CCC1, C[j*16:(j+1)*16])
 	}
 	copy(C[0:16], CCC1)
 
@@ -108,8 +110,7 @@ func TransformEME32(bc cipher.Block, T []byte, P []byte, direction int) (C []byt
 		/* CCj = AES-enc(K; CCCj) */
 		transformAES(C[j*16:(j+1)*16], C[j*16:(j+1)*16], direction, bc)
 		/* Cj = 2**(j-1)*L xor CCj */
-		Cj := xorBlocks(C[j*16:(j+1)*16], L)
-		copy(C[j*16:(j+1)*16], Cj)
+		xorBlocks(C[j*16:(j+1)*16], C[j*16:(j+1)*16], L)
 		L = multByTwo(L)
 	}
 
